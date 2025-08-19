d2e2f5a249b6187aa8636c7cdf985db56c07a0dd
ChitawornR <chitaworn.rr@gmail.com>
2025-08-19 12:23:34 +0700

    done findWinner and set useEffect to alert winner

--- frontend/src/components/boardGame/BoardGame.jsx
+++ frontend/src/components/boardGame/BoardGame.jsx
@@ -14,70 +14,93 @@ function BoardGame() {
   const [board, setBoard] = useState(emptyBoard);
   const [player, setPlayer] = useState("X");
   const [moves, setMoves] = useState([]);
+  const [winner, setWinner] = useState("");
 
-  useEffect(()=>{
-    const win = findWinner(board,size === 3?3:4)
-    console.log(win);
-  })
+  useEffect(() => {
+    const win = findWinner(board, size === 3 ? 3 : 4);
+    if (win) {
+      setWinner(win);
+    }
+  }, [board]);
 
-  function findWinner(currentBoard,steak){
+  useEffect(() => {
+    /* 
+      this useEffect create for wait board set last player to cell
+      if winner then alert message and set zero
+    */
+    if (!winner) return;
+    const id = setTimeout(() => {
+      alert(`Winner is ${winner}`);
+      setBoard(emptyBoard);
+      setMoves([]);
+      setPlayer("X");
+      setWinner("");
+    }, 200);
+    return () => clearTimeout(id);
+  }, [winner]);
+
+  function findWinner(currentBoard, steak) {
+    /* 
+    this function is return winner 
+    value is null,X or O
+  */
     // check row
-    for(let row = 0; row < size; row++){
-      for(let col = 0; col <= size-steak; col++){
-        let temp = []
-        for(let i = 0; i < steak; i++){
-          temp.push(currentBoard[row][col+i])
+    for (let row = 0; row < size; row++) {
+      for (let col = 0; col <= size - steak; col++) {
+        let temp = [];
+        for (let i = 0; i < steak; i++) {
+          temp.push(currentBoard[row][col + i]);
         }
-        if(temp.every((cell)=> (cell === temp[0] && cell != ''))){
-          return temp[0]
+        if (temp.every((cell) => cell === temp[0] && cell != "")) {
+          return temp[0];
         }
       }
     }
 
     // check col
-    for(let col = 0; col < size; col++){
-      for(let row = 0; row <= size-steak; row++){
-        let temp = []
-        for(let i = 0; i < steak; i++){
-          temp.push(currentBoard[row+i][col])
+    for (let col = 0; col < size; col++) {
+      for (let row = 0; row <= size - steak; row++) {
+        let temp = [];
+        for (let i = 0; i < steak; i++) {
+          temp.push(currentBoard[row + i][col]);
         }
-        if(temp.every((cell)=> (cell === temp[0] && cell != ''))){
-          return temp[0]
+        if (temp.every((cell) => cell === temp[0] && cell != "")) {
+          return temp[0];
         }
       }
     }
 
     // Diagonal right to left
-    for(let row = 0; row <= size - steak; row++){
-      for(let col = 0; col <= size - steak; col++){
-        let temp = []
-        for(let i = 0; i < steak; i++){
-          temp.push(currentBoard[row+i][col+i])
+    for (let row = 0; row <= size - steak; row++) {
+      for (let col = 0; col <= size - steak; col++) {
+        let temp = [];
+        for (let i = 0; i < steak; i++) {
+          temp.push(currentBoard[row + i][col + i]);
         }
-        if(temp.every((cell)=>(cell == temp[0] && cell !== ''))){
-          return temp[0]
+        if (temp.every((cell) => cell == temp[0] && cell !== "")) {
+          return temp[0];
         }
       }
     }
 
     // Diagonal left to right
-    for(let row = 0; row <= size - steak; row++){
-      for(let col = size - 1; col >= size - steak; col--){
-        let temp = []
-        for(let i = 0; i < steak; i++){
-          temp.push(currentBoard[row+i][col-i])
+    for (let row = 0; row <= size - steak; row++) {
+      for (let col = size - 1; col >= size - steak; col--) {
+        let temp = [];
+        for (let i = 0; i < steak; i++) {
+          temp.push(currentBoard[row + i][col - i]);
         }
-        if(temp.every((cell)=>(cell == temp[0] && cell !== ''))){
-          return temp[0]
+        if (temp.every((cell) => cell == temp[0] && cell !== "")) {
+          return temp[0];
         }
       }
     }
 
-    return null
+    return null;
   }
 
   function handleOnClickCell(row, col) {
-    if(board[row][col] !== '') return
+    if (board[row][col] !== "" || winner) return;
     const tempBoard = board.map((r, i) =>
       r.map((c, j) => {
         if (i === row && j === col) {
@@ -91,25 +114,29 @@ function BoardGame() {
     setMoves(move);
     setBoard(tempBoard);
     setPlayer(player === "X" ? "O" : "X");
+
+
   }
 
   return (
-    <div
-      className="boardGame"
-      style={{ gridTemplateColumns: `repeat(${size},1fr)` }}
-    >
-      {board.map((row, i) =>
-        row.map((cell, j) => (
-          <div
-            onClick={() => handleOnClickCell(i, j)}
-            className="cell"
-            key={`${i},${j}`}
-          >
-            {cell}
-          </div>
-        ))
-      )}
-    </div>
+    <>
+      <div
+        className="boardGame"
+        style={{ gridTemplateColumns: `repeat(${size},1fr)` }}
+      >
+        {board.map((row, i) =>
+          row.map((cell, j) => (
+            <div
+              onClick={() => handleOnClickCell(i, j)}
+              className="cell"
+              key={`${i},${j}`}
+            >
+              {cell}
+            </div>
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
